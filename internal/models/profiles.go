c68c9374cb64ccfe91cc9599801ec4bcc6b0e045
faizallmaullana <faizalmaulanasekias@gmail.com>
2025-12-26 14:03:11 +0700

    add database connection provider and user/profile model encryption

--- internal/models/profiles.go
+++ internal/models/profiles.go
@@ -8,19 +8,48 @@ import (
 )
 
 type Profile struct {
-	ID       uuid.UUID `gorm:"type:uuid;primaryKey" json:"id"`
-	UserID   uuid.UUID `gorm:"type:uuid;not null" json:"user_id"`
-	NIK      string    `gorm:"unique;not null" json:"nik"`
-	Phone    string    `json:"phone"`
-	Religion string    `json:"religion"`
-	Address  string    `json:"address"`
-	Work     string    `json:"work"`
-	Name     string    `json:"name"`
+	ID          uuid.UUID `gorm:"type:uuid;primaryKey" json:"-"`
+	EncryptedID string    `gorm:"-" json:"id,omitempty"`
+	UserID      uuid.UUID `gorm:"type:uuid;not null" json:"user_id"`
+	NIK         string    `gorm:"unique;not null" json:"nik"`
+	Phone       string    `json:"phone"`
+	Religion    string    `json:"religion"`
+	Address     string    `json:"address"`
+	Work        string    `json:"work"`
+	Name        string    `json:"name"`
 }
 
 func (p *Profile) BeforeCreate(tx *gorm.DB) (err error) {
 	if p.ID == uuid.Nil {
 		p.ID = utils.GenerateUUIDV6()
 	}
+	return nil
+}
+
+func (p *Profile) BeforeFind(tx *gorm.DB) (err error) {
+	if p.EncryptedID != "" {
+		key, err := utils.GetEncryptKey()
+		if err != nil {
+			return err
+		}
+		id, err := utils.DecryptToUUID(p.EncryptedID, key)
+		if err != nil {
+			return err
+		}
+		tx.Where("id = ?", id)
+	}
+	return nil
+}
+
+func (p *Profile) AfterFind(tx *gorm.DB) (err error) {
+	key, err := utils.GetEncryptKey()
+	if err != nil {
+		return err
+	}
+	enc, err := utils.EncryptUUID(p.ID, key)
+	if err != nil {
+		return err
+	}
+	p.EncryptedID = enc
 	return nil
 }
--- internal/models/users.go
+++ internal/models/users.go
@@ -10,7 +10,8 @@ import (
 )
 
 type User struct {
-	ID           uuid.UUID `gorm:"type:uuid;primaryKey" json:"id"`
+	ID           uuid.UUID `gorm:"type:uuid;primaryKey" json:"-"`
+	EncryptedID  string    `gorm:"-" json:"id,omitempty"`
 	Email        string    `gorm:"unique;not null" json:"email"`
 	PasswordHash string    `gorm:"not null" json:"-"`
 	CreatedAt    time.Time `gorm:"autoCreateTime" json:"created_at"`
@@ -21,5 +22,34 @@ func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
 	if u.ID == uuid.Nil {
 		u.ID = utils.GenerateUUIDV6()
 	}
+	return nil
+}
+
+func (u *User) BeforeFind(tx *gorm.DB) (err error) {
+	if u.EncryptedID != "" {
+		key, err := utils.GetEncryptKey()
+		if err != nil {
+			return err
+		}
+		id, err := utils.DecryptToUUID(u.EncryptedID, key)
+		if err != nil {
+			return err
+		}
+		// modify query to search by decrypted uuid
+		tx.Where("id = ?", id)
+	}
+	return nil
+}
+
+func (u *User) AfterFind(tx *gorm.DB) (err error) {
+	key, err := utils.GetEncryptKey()
+	if err != nil {
+		return err
+	}
+	enc, err := utils.EncryptUUID(u.ID, key)
+	if err != nil {
+		return err
+	}
+	u.EncryptedID = enc
 	return nil
 }
